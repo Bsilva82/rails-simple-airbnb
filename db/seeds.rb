800a6633d769869e16d56e0273f7400cb870b0ba
Bruno Silva <info.bsilva@gmail.com>
2021-03-20 18:08:37 +0100

    create seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,40 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "Delete the database"
+
+Flat.destroy_all
+
+puts "Creating new flats"
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Ibis',
+  address: 'Lausanne',
+  description: 'In the middle of the city, near the lake',
+  price_per_night: 150,
+  number_of_guests: 4
+)
+
+Flat.create!(
+  name: 'La Verbiere',
+  address: 'Verbier',
+  description: 'Near the ski station in the middle of the swiss Alps',
+  price_per_night: 600,
+  number_of_guests: 2
+)
+
+Flat.create!(
+  name: 'Chez Nadine',
+  address: 'Carouge, Genève',
+  description: 'With a beautiful view on the lake ',
+  price_per_night: 400,
+  number_of_guests: 5
+)
+
+
+puts "#{Flat.count} Flats created"
